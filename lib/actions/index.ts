9e72f6129aa7dd551a0016cb95d0d8a454120512
sandali jaiswal <sandalijaiswal50@gmail.com>
2023-12-13 01:12:11 +0530

    3rd Day

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,1 +1,13 @@
 "use server"
+
+export async function scrapAndStoreProduct(productUrl: string){
+    if(!productUrl) return;
+
+    try{
+       const scrapedProduct = await scrapAmazonProduct(productUrl);
+    } catch (error: any){
+
+      throw new Error(`Sailed to create/update product: ${error.message}`)
+    }
+    
+}
